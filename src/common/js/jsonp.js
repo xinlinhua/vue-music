ecae7e59c17044a96e66548089d8c09f85489001
Your Name <you@example.com>
2017-10-27 18:12:34 +0800

    add

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,25 @@
+import originJsonp from 'jsonp'
+
+export default function jsonp(url, data, option) {
+    url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
+    return new Promise((resolve, reject) => {
+        originJsonp(url, option, (err, data) => {
+            if (!err) {
+                resolve(data)
+            } else {
+                reject(err)
+            }
+        })
+    })
+}
+
+function param(data) {
+    let url = '';
+
+    for (var key in data) {
+        let value = !!data[key] ? data[key] : '';
+        url += '&' + key + '=' + encodeURIComponent(value)
+    }
+
+    return url ? url.substring(1) : ''
+}
