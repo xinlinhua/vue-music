865f297a0f0bbb5cfb9801479c116c91e3e96df8
xinlinhua <huaxl1020@qq.com>
2018-01-01 20:28:53 +0800

    20180101

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -12,16 +12,35 @@
                     <h1 class="title" v-html="currentSong.name"></h1>
                     <h2 class="subtitle" v-html="currentSong.singer"></h2>
                 </div>
-                <div class="middle">
-                    <div class="middle-l">
+                <div class="middle"  
+                @touchstart.prevent="middleTouchStart" 
+                @touchmove.prevent="middleTouchMove"
+                @touchend="middleTouchEnd">>
+                    <div class="middle-l" ref="middleL">
                         <div class="cd-wrapper" ref="cdWrapper">
                             <div class="cd" :class="cdCls">
                                 <img class="image" alt="" :src="currentSong.image">
                             </div>
                         </div>
                     </div>
+                    <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">
+                        <div class="lyric-wrapper">
+                            <div v-if="currentLyric" >
+                                <p v-for="(lyric,index) in currentLyric.lines" :key="index"
+                                 :class="{'current': currentLineNum === index}"
+                                  class="text" 
+                                  ref="lyricLine">
+                                    {{lyric.txt}}
+                                </p>
+                            </div>
+                        </div>
+                    </scroll>
                 </div>
                 <div class="bottom">
+                    <div class="dot-wrapper">
+                        <span class="dot" :class="{'active': currentShow === 'cd'}"></span>
+                        <span class="dot" :class="{'active': currentShow === 'lyric'}"></span>
+                    </div>
                     <div class="progress-wrapper">
                         <span class="time time-l">{{format(currentTime)}}</span>
                         <div class="progress-bar-wrapper">
@@ -79,25 +98,31 @@ import animations from 'create-keyframe-animation'
 import {prefixStyle} from 'common/js/dom'
 import ProgressBar from 'base/progress-bar/progress-bar'
 import ProgressCircle from 'base/progress-circle/progress-circle'
-const transform = prefixStyle('transform')
 import {playMode} from 'common/js/config'
 import {shuffle} from 'common/js/util'
 import LyricParser from 'lyric-parser'
+import Scroll from 'base/scroll/scroll'
+const transform = prefixStyle('transform')
+const transitionDuraction = prefixStyle('transitionDuraction')
 export default {
     data(){
         return {
             songReady: false,
             currentTime: 0,
             radius: 32,
-            currentLyric: []
+            currentLyric: [],
+            currentLineNum: 0,
+            currentShow: 'cd'
         }
     },
-    mounted(){
-     
+    
+    created(){
+        this.touch = {}
     },
     components:{
         ProgressBar,
-        ProgressCircle
+        ProgressCircle,
+        Scroll
     },
     computed:{
         playIcon(){
@@ -130,6 +155,65 @@ export default {
         ])
     },
     methods:{
+        middleTouchStart(e){
+            this.touch.initiated = true
+            const touch = e.touches[0]
+            this.touch.startX = touch.pageX
+            this.touch.startY = touch.pageY
+        },
+        middleTouchMove(e){
+           
+           if(!this.touch.initiated){
+               return
+           }
+          
+            const touch = e.touches[0]
+            const deltaX = touch.pageX - this.touch.startX
+            const deltaY = touch.pageY - this.touch.startY
+            if(Math.abs(deltaY) > Math.abs(deltaX)){
+                return
+            }
+           
+            const left = this.currentShow === "cd" ? 0 : -window.innerWidth
+            const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left+deltaX))
+            this.touch.percent = Math.abs(offsetWidth/window.innerWidth)
+            this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px,0,0)`
+            this.$refs.lyricList.$el.style[transitionDuraction] = 0 
+            this.$refs.middleL.style.opacity = 1 -  this.touch.percent
+            this.$refs.middleL.style[transitionDuraction] = 0 
+
+        },
+        middleTouchEnd(e){
+            let offsetWidth 
+            let opacity
+            const time = 300
+            if(this.currentShow === 'cd'){
+                
+                if(this.touch.percent > 0.1){
+                    offsetWidth = -window.innerWidth
+                    this.currentShow  = 'lyric'
+                    opacity = 0
+                }else{
+                    offsetWidth = 0 
+                    opacity = 1
+                }
+            }else{
+                    if(this.touch.percent < 0.9){
+                    offsetWidth = 0
+                    this.currentShow  = 'cd'
+                    opacity = 1
+                }else{
+                    offsetWidth = -window.innerWidth
+                    opacity = 0
+                }
+            }
+
+            this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px,0,0)`
+            this.$refs.lyricList.$el.style[transitionDuraction] = `${time}ms`
+            this.$refs.middleL.style.opacity = opacity
+            this.$refs.middleL.style[transitionDuraction] = `${time}ms` 
+            this.touch.initiated = false
+        },
         back(){
             this.setFullScreen(false)
         },
@@ -255,9 +339,22 @@ export default {
         },
         getLyric(){
             this.currentSong.getLyric().then((res)=>{
-                this.currentLyric = new LyricParser(res)
-                console.log(this.currentLyric )
+                this.currentLyric = new LyricParser(res,this.handelLyric)
+                    if(this.playing){
+                        this.currentLyric.play()
+                    }
+              
             })
+        },
+        handelLyric({lineNum,txt} ){
+            console.log(lineNum)
+            this.currentLineNum = lineNum
+            if(lineNum > 5){
+                let lineEl = this.$refs.lyricLine[lineNum -5]
+                this.$refs.lyricList.scrollToElement(lineEl, 1000)
+            }else {
+                this.$refs.lyricList.scrollTo(0,0, 1000)
+            }
         },
         _pad(num,n=2){
             let len = num.toString().length;
